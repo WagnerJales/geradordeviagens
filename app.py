4ccdd362c9f209c0b845bdb12f4d6b8e0e8ec34a
WagnerJales <100356581+WagnerJales@users.noreply.github.com>
2025-05-28 09:27:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ with col_logo[0]:
 st.markdown("""
     <style>
         h1, h2, h3 {
-            font-size: 85% !important;
+            font-size: 95% !important;
         }
         .stMetric {
             font-size: 1.2em !important;
